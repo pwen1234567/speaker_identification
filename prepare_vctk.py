9ddadbf03fe5690ace832cb72d348fa6a69d4da1
pwen1234567 <pwen1234567@gmail.com>
2020-01-02 01:31:36 +0800

    Fix UnicodeEncodeError.

--- prepare_vctk.py
+++ prepare_vctk.py
@@ -5,7 +5,7 @@ import random
 import shutil
 
 # Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 50, fill = '█', printEnd = "\r"):
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 50, fill = '#', printEnd = "\r"):
     """
     Call in a loop to create terminal progress bar
     @params:
